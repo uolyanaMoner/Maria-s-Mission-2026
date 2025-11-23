557c3eb62174f95f1e1207ddb103d23b76e4aa9a
Uolyana Moner <98236055+uolyanaMoner@users.noreply.github.com>
2025-11-23 23:56:01 +0200

    edits

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Maria-s-Mission-2026/' // مهم جدًا للنشر على GitHub Pages
+  base: '/Maria-s-Mission-2026/'  // مهم جدًا للنشر على GitHub Pages
 })
