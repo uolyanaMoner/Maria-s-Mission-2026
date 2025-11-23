16c266eb1cf7d0bbe4348a243c9eca7d3bf86e42
Uolyana Moner <98236055+uolyanaMoner@users.noreply.github.com>
2025-11-24 00:01:05 +0200

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";  // ← لازم يكون موجود
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/Maria-s-Mission-2026">
