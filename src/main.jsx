2138464754e7802e4096aac64f5cbe9c9974e949
Uolyana Moner <98236055+uolyanaMoner@users.noreply.github.com>
2025-11-23 23:44:52 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter basename="/Maria-s-Mission-2026">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
