2a3ab7d3ef30ebbb81de07a31fff0f7ba3168238
Uolyana Moner <98236055+uolyanaMoner@users.noreply.github.com>
2025-11-24 19:55:14 +0200

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Maria-s-Mission-2026/' // مهم للنشر على GitHub Pages
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/Maria-s-Mission-2026/' // مهم للنشر على GitHub Pages
-});
+  build: {
+    outDir: 'dist', // المجلد النهائي بعد build
+  },
+})
